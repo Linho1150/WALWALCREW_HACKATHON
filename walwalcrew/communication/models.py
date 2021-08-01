44a1cf6bfb37c31a23db97f76293b4bbc11b533e
linho1150 <56428918+Linho1150@users.noreply.github.com>
2021-08-01 16:20:13 +0900

    update comment
    add comment model with foreign key
    add comment additional code

--- walwalcrew/communication/models.py
+++ walwalcrew/communication/models.py
@@ -5,5 +5,16 @@ class question_list(models.Model):
     id = models.AutoField(primary_key=True)
     cateogry = models.CharField(max_length=50)
     title = models.CharField(max_length=200)
+    nickname = models.CharField(max_length=100, null=True)
     text = models.CharField(max_length=5000)
     answer = models.CharField(max_length=5)
+    date = models.DateField(auto_now_add=True, blank=True)
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    question_id = models.ForeignKey("question_list", related_name="question_id", on_delete=models.CASCADE, db_column="question_id")
+    text = models.CharField(max_length=5000,null=False)
+    nickname = models.CharField(max_length=100, null=True)
+    like = models.BigIntegerField()
+    unlike = models.BigIntegerField()
+    date = models.DateField(auto_now_add=True, blank=True)
--- walwalcrew/communication/views.py
+++ walwalcrew/communication/views.py
@@ -1,6 +1,6 @@
 from typing import Text
 from django.shortcuts import render
-from .models import question_list
+from .models import Comment, question_list
 from django.core import serializers
 
 # Create your views here.
@@ -11,10 +11,11 @@ def list(request):
 
 def add(request):
     if request.method == 'POST':
-        if request.POST.get('title') and request.POST.get('text') and request.POST.get('cateogry') and request.POST.get('answer'):
+        if request.POST.get('title') and request.POST.get('nickname') and request.POST.get('text') and request.POST.get('cateogry') and request.POST.get('answer'):
             post=question_list()
             post.cateogry= request.POST.get('cateogry')
             post.title= request.POST.get('title')
+            post.nickname= request.POST.get('nickname')
             post.text= request.POST.get('text')
             post.answer= request.POST.get('answer')
             post.save()
@@ -24,5 +25,6 @@ def add(request):
 
 def detail(request,question_id):
     questions = serializers.serialize("json", question_list.objects.filter(id=question_id))
-    data = {"questions": questions}
+    comment = Comment.objects.filter(question_id=question_id)
+    data = {"questions": questions, "comment":comment}
     return render(request,'sub.html',data)
