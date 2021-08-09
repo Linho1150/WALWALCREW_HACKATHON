dc8af4d034974c4e3203c0d1a2f32d54a7cc70d7
linho1150 <56428918+Linho1150@users.noreply.github.com>
2021-08-09 16:18:06 +0900

    Important upgrad ( ajax request post & div part refresh)

--- walwalcrew/communication/views.py
+++ walwalcrew/communication/views.py
@@ -2,6 +2,7 @@ from typing import Text
 from django.shortcuts import redirect, render
 from .models import Comment, question_list
 from django.core import serializers
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def list(request):
@@ -20,6 +21,8 @@ def add(request):
             post.answer= request.POST.get('answer')
             post.save()
             return redirect('/comm/')
+        else:
+            print(request)
     else:
         return render(request,'index.html')
 
@@ -28,14 +31,31 @@ def detail(request,question_id):
     comment = Comment.objects.filter(question_id=question_id)
     data = {"questions": questions, "comment":comment}
     if request.method == 'POST':
-        if request.POST.get('text') and request.POST.get('nickname'):
+        print(request.POST)
+        if request.POST.get('text') and request.POST.get('nick'):
             post=Comment()
-            post.nickname= request.POST.get('nickname')
+            post.nickname= request.POST.get('nick')
             post.text= request.POST.get('text')
             post.question_id=question_list(id=question_id)
             post.like=0
             post.unlike=0
             post.save()
+            return render(request,'sub.html',data)
+
+        elif request.POST.get('finger_id') and request.POST.get('finger_text'):
+            finger = Comment.objects.get(id=request.POST.get('finger_id'))
+            finger.like = int(request.POST.get('finger_text'))+1
+            finger.save()
+            return render(request,'sub.html',data)
+
+        elif request.POST.get('cnt'):
+            vote = question_list.objects.get(id=question_id)
+            oriText = question_list.objects.get(id=question_id).answer
+            tmpList=oriText.split('#')
+            count = int(request.POST.get('cnt'))
+            tmpList[count]=str(int(tmpList[count])+1)
+            vote.answer="#".join(tmpList)
+            vote.save()
             return render(request,'sub.html',data)
     else:
         return render(request,'sub.html',data)
